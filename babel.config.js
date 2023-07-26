4270d58dabbf0a26c50e04f213dad5ad99223c0e
D.hamza <hamzademnati21@gmail.com>
2023-07-26 14:57:47 +0200

    setup tailwind css & and alias for folder stractur

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+// babel.config.js
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            "@": "./src",
+            "@screens": "./src/screens", // Add this alias for your screens directory
+          },
+        },
+      ],
+    ],
+
   };
 };
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function HomeScreen() {
+    return (
+        <View className="bg-black">
+            <Text className="text-white">Home Screen</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
